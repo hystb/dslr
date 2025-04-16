fa1264e0230e1cc4945129b46928ba9e2a39f27c
nmilan <milan.nicolas@rocketmail.com>
2025-04-16 20:35:15 +0200

    add scatter plots script

---
+++ scatter_plot.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append('./src/preprocessing/file')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from dataset import DataSet
+from loader import Loader
+
+
+def do_scatter(dataset: DataSet):
+    loader = Loader()
+    dataset = loader.load("./datasets/dataset_train.csv")
+    data = dataset.get_all()
+    headers = dataset.get_headers()
+
+    x_course = "Astronomy"
+    y_course = "Defense Against the Dark Arts"
+
+    idx_x = headers[x_course]
+    idx_y = headers[y_course]
+    idx_house = headers["Hogwarts House"]
+
+    colors = {
+        "Gryffindor": "red",
+        "Ravenclaw": "blue",
+        "Hufflepuff": "gold",
+        "Slytherin": "green"
+    }
+
+    plt.figure(figsize=(6, 6))
+    for house, color in colors.items():
+        mask_house = data[:, idx_house] == house
+        x_vals = data[mask_house, idx_x]
+        y_vals = data[mask_house, idx_y]
+
+        mask_valid = (x_vals != "") & (y_vals != "")
+        x_vals = x_vals[mask_valid].astype(float)
+        y_vals = y_vals[mask_valid].astype(float)
+
+        plt.scatter(x_vals, y_vals, alpha=0.5, label=house, color=color, s=10)
+
+    plt.xlabel(x_course)
+    plt.ylabel(y_course)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    if (len(sys.argv) == 2):
+        dataset = Loader().load(sys.argv[1])
+        do_scatter(dataset)
+
+
+if __name__ == "__main__":
+    main()
